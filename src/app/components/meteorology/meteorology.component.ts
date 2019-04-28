285e75c22711b16c3b2e789c2f8582dede4af952
EstevDuart <estev.duart.2@gmail.com>
2019-04-27 23:06:03 -0300

    chart gradient color

--- src/app/components/meteorology/meteorology.component.ts
+++ src/app/components/meteorology/meteorology.component.ts
@@ -18,17 +18,20 @@ export class MeteorologyComponent implements OnInit {
   ] 
 
 
-  
+  colors = [];
+  hoverColors = [];
+  barDatas = []; //[28, 48, 40, 19, 86, 27, 90];
 
   barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true,
   };
-  barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014'];
   barChartType = 'bar';
   barChartLegend = true;
   barChartData = [
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Intensidade de Chuva', 
+    {data: [], 
+      label: 'Intensidade de Chuva', 
       
       /*
         esses 4 atributos devem ser alterados.
@@ -37,10 +40,10 @@ export class MeteorologyComponent implements OnInit {
         No hover, ambas estao cinza
         Falta adicionar os outros  indices, com a progressao de cores correta para cada um
       */
-      backgroundColor: ['rgba(0, 200, 200, 1)', 'rgba(200, 0, 200, 1)'],
-      borderColor: ['rgba(0, 200, 200, 1)', 'rgba(200, 0, 200, 1)'],
-      hoverBackgroundColor: ['rgba(150, 150, 150, 1)', 'rgba(150, 150, 150, 1)'],
-      hoverBorderColor: ['rgba(150, 150, 150, 1)', 'rgba(150, 150, 150, 1)']
+      backgroundColor: [], //['rgba(0, 200, 200, 1)', 'rgba(200, 0, 200, 1)'],
+      borderColor: [], //['rgba(0, 200, 200, 1)', 'rgba(200, 0, 200, 1)'],
+      hoverBackgroundColor: [], //['rgba(150, 150, 150, 1)', 'rgba(150, 150, 150, 1)'],
+      hoverBorderColor: [] //['rgba(150, 150, 150, 1)', 'rgba(150, 150, 150, 1)']
     }
   ];
 
@@ -49,5 +52,38 @@ export class MeteorologyComponent implements OnInit {
    }
 
   ngOnInit() {
+    this.setBarDatas();
+    this.createColors();
+  }
+
+  setBarDatas() {
+    let len = 9;
+    for (let i=0; i<len; i++) {
+      this.barDatas.push(Math.round(Math.random()*10));
+    }
+
+    this.barChartData[0].data = this.barDatas;
+  }
+
+  createColors() {
+    this.colors = [];
+    this.hoverColors = [];
+
+    let min = 50;
+    let max = 240;
+
+    let r = 0;
+    let g = min;
+    let b = min;
+    
+    let n = this.barDatas.length
+    for (let i=0; i<n; i++) {
+      let val = this.barDatas[i];
+      val = min + (max - min)*(val/n);
+      g = b = val;
+      this.colors.push('rgba('+r+', '+g+', '+b+', 1)');
+    }
+
+    this.barChartData[0].backgroundColor = this.colors;
   }
 }
