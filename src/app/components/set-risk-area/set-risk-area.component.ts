e1bc279722b3fa44458ebf84626f810ade70c2d2
GHMarques <gustavo.marques2011@gmail.com>
2019-05-30 20:17:17 -0300

    add set risk area

--- src/app/components/set-risk-area/set-risk-area.component.ts
+++ src/app/components/set-risk-area/set-risk-area.component.ts
@@ -13,29 +13,24 @@ export class SetRiskAreaComponent implements OnInit {
 
   public map: any;
   public draw: any;
+  public lat: number;
+  public lng: number;
   constructor() { }
   riskAreaForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required
-    ]),
-    inclination: new FormControl('', [
-      Validators.required
-    ]),
-    high: new FormControl('', [
-      Validators.required
-    ]),
-    potential: new FormControl('', [
+    area: new FormControl('', [
       Validators.required
     ])
   });
 
   ngOnInit() {
+    this.lat = -43.999111399648655;
+    this.lng = -19.938555045090476;
     mapboxgl.accessToken = 'pk.eyJ1IjoiZ2htYXJxdWVzIiwiYSI6ImNqdXp0YWJnczFibGYzeXJ2empwem91b28ifQ.1m8Dtl9e1xtBrUgzVYHSmg';
     /* eslint-disable */
     this.map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'mapbox://styles/mapbox/streets-v9', //hosted style id
-      center: [-43.999111399648655, -19.938555045090476], // starting position
+      center: [this.lat, this.lng], // starting position
       zoom: 17// starting zoom
       });
     /* eslint-disable */
@@ -63,20 +58,41 @@ export class SetRiskAreaComponent implements OnInit {
     
     this.map.on('load', function (map) {
       map.target.addLayer({
-        'id': 'maine',
+        'id': 'riskArea',
         'type': 'fill',
         'source': {
           'type': 'geojson',
           'data': {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [[
-                [-43.999200, -19.938327],
-                [-43.999200, -19.938700],
-                [-43.998803, -19.938700],
-                [-43.998819, -19.938327]]]
-            }
+            'type': 'FeatureCollection',
+            'features': [{
+              'type': 'Feature',
+              'properties': {
+                'name': 'CEFET - Campus 2',
+                'risk': '1'
+              },
+              'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[
+                  [-43.999200, -19.938327],
+                  [-43.999200, -19.938700],
+                  [-43.998803, -19.938700],
+                  [-43.998819, -19.938327]]]
+              }
+            }, {
+              'type': 'Feature',
+              'properties': {
+                'name': 'CEFET - Campus 1',
+                'risk': '2'
+              },
+              'geometry': {
+                'type': 'Polygon',
+                'coordinates': [[
+                  [-43.977809, -19.929904],
+                  [-43.977711, -19.930382],
+                  [-43.976471, -19.930202],
+                  [-43.976649, -19.929681]]]
+              }
+              }]
           }
         },
         'layout': {},
@@ -85,6 +101,22 @@ export class SetRiskAreaComponent implements OnInit {
           'fill-opacity': 0.3
         }
       });
+
+      map.target.on('mouseenter', 'riskArea', function () {
+        map.target.getCanvas().style.cursor = 'pointer';
+      });
+
+      map.target.on('mouseleave', 'riskArea', function () {
+        map.target.getCanvas().style.cursor = '';
+      });
+
+      map.target.on('click', 'riskArea', function (e) {
+        console.log(e.features[0]);
+        new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML('<b>Nome:</b> ' + e.features[0].properties.name + '<br/>' + '<b>Risco: </b>' + e.features[0].properties.risk)
+        .addTo(map.target);
+        });
     });
   }
 
@@ -94,5 +126,21 @@ export class SetRiskAreaComponent implements OnInit {
   forwardGeocoder(query) {
     var matchingFeatures = [];
     return matchingFeatures;
+  }
+
+  changeCenter(e){
+    if(e.value == 1){
+      this.lat = -43.999111399648655;
+      this.lng = -19.938555045090476;
+    } else {
+      this.lat = -43.977683;
+      this.lng = -19.930298;
+    }
+    
+    this.map.flyTo({
+      center: [
+        this.lat,
+        this.lng]
+      });
   }
 }
